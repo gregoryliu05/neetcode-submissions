28927102480d9199dd1e99fb8a077ecf76c25141
Gregory Liu <115047521+gregoryliu05@users.noreply.github.com>
2026-09-01 01:43:40 -0700

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, value = "", isWordEnd = False):
+        self.value = value
+        self.isWordEnd= isWordEnd
+        self.nextValues = dict()
+class PrefixTree:
+    # how can i determine word vs just a prefix?
+    # if we instert word and then call startsWith(word) imma assume false.
+
+    # 1. dictionary of words
+    # 2. or a node class with isWordEnd field
+
+    # key-value pairs 
+
+    # value, nextChars, isWordEnd
+
+
+
+    def __init__(self):
+        self.trie = Node()
+        
+
+    def insert(self, word: str) -> None:
+        trie = self.trie
+        for i, c in enumerate(word):
+            if c not in trie.nextValues:
+                trie.nextValues[c] = Node(c)
+            if i == len(word) - 1:
+                trie.nextValues[c].isWordEnd = True
+            trie = trie.nextValues[c]
+
+
+    def search(self, word: str) -> bool:
+        trie = self.trie
+        for i, c in enumerate(word):
+            if c not in trie.nextValues:
+                return False
+            trie = trie.nextValues[c]
+        return trie.isWordEnd
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        trie = self.trie
+        for i, c in enumerate(prefix):
+            if c not in trie.nextValues:
+                return False
+            trie = trie.nextValues[c]
+        return True
+        
+        
