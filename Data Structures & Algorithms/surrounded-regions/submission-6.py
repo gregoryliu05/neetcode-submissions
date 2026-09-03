d85fc4e4dd7a8f22552bd0412479fdd769870ae7
Gregory Liu <115047521+gregoryliu05@users.noreply.github.com>
2026-09-03 09:41:55 -0700

    Add: surrounded-regions - submission-6

---
+++ Data Structures & Algorithms/surrounded-regions/submission-6.py
@@ -0,0 +1,44 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        # edges
+        # do a bfs or dfs if we see an O
+        # if none of those O reach an edge we replace all those with x's
+        rows = len(board)
+        cols = len(board[0])
+        globalseen = set()
+        rowEdges = set([0, rows-1])
+        colEdges = set([0, cols-1])
+        dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+        def bfs(r,c):
+            seen = set([(r,c)])
+
+            queue = deque()
+            queue.append((r,c))
+            while queue:
+                cr, cc = queue.popleft()
+                seen.add((cr,cc))
+                if cr in rowEdges or cc in colEdges:
+                    return set()
+                for dr, dc in dirs:
+                    nr,nc = cr + dr, cc + dc 
+                    if nr >= 0 and nr < rows and nc >= 0 and nc < cols:
+                        notused = (nr,nc) not in seen
+                        isO = board[nr][nc] == "O"
+                        if (nr,nc) not in seen and board[nr][nc] == "O":
+                            queue.append((nr,nc))
+                            seen.add((nr,nc))
+
+            return seen
+
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) not in globalseen and board[r][c] == "O":
+                    indices = bfs(r,c)
+                    if len(indices) > 0:
+                        for x,y in indices:
+                            board[x][y] = "X"
+                    else:
+                        globalseen.add((r,c))
+                    
+
+        
