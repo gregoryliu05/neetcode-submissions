2471bdddbcf0f39154e23fc7bfe0138344701f74
Gregory Liu <115047521+gregoryliu05@users.noreply.github.com>
2026-09-04 00:59:59 -0700

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # directed graph
+        # [a,b] -> means have to take b before taking a
+        # b -> a
+
+        adj = [[] for _ in range(numCourses)]
+        for a,b in prerequisites:
+            adj[b].append(a)
+        # if there is a cycle its not valid essentially 
+        # there can be multiple connected components
+        # so we have to check all of the nodes to see if any lead to a cycle
+        # a -> b -> c -> d ->a
+        #       -> e -> f-> g
+
+        def dfs(node, seen):
+            if adj[node] == []:
+                return True
+            
+            for nxt in adj[node]:
+                if nxt in seen:
+                    return False
+                seen.add(nxt)
+                if not dfs(nxt, seen):
+                    return False
+                seen.remove(nxt)
+            print(node)
+            adj[node] = []
+            return True
+            
+
+        for i in range(numCourses):
+            if not (dfs(i, set([i]))):
+                return False
+
+
+        return True
+        
